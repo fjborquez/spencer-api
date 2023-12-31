c9a9e8b3207311a36b0d52a87090366495402076
Francisco Borquez <fborquez@outlook.com>
2023-12-31 20:02:02 -0300

    Sorting annual results

--- app/Http/Controllers/ResultadosAnualesEmpresa.php
+++ app/Http/Controllers/ResultadosAnualesEmpresa.php
@@ -21,12 +21,14 @@ class ResultadosAnualesEmpresa extends Controller
             $formsInJson[] = $formService->formToJson($code);
         }
 
-        return usort($formsInJson, function ($current, $next) {
+        usort($formsInJson, function ($current, $next) {
             if ($current['endOfPeriod'] === $next['endOfPeriod']) {
                 return 0;
             }
 
             return $current['endOfPeriod'] < $next['endOfPeriod'] ? -1 : 1;
         });
+
+        return $formsInJson;
     }
 }
